521ca5a0d436ba2c2b32ae684fd370f21defdb28
vineboneto <vineboneto@gmail.com>
2022-02-07 21:56:17 -0300

    feat: layout sidebar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,10 +1,13 @@
-import { Header } from '@/components'
+import { Flex } from '@chakra-ui/react'
+import { Header, Sidebar } from '@/components'
 
 export default function Dashboard() {
   return (
-    <>
+    <Flex direction="column">
       <Header />
-      {/* <h1>Dashboard</h1> */}
-    </>
+      <Flex width="100%" my="6" maxWidth="1480" mx="auto" px="6">
+        <Sidebar />
+      </Flex>
+    </Flex>
   )
 }
